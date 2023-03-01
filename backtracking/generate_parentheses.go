5758f3a1998a83486a20a69a1af1ea7b28256414
stephenwongvdx <stephen91789443@gmail.com>
2023-03-01 14:54:33 +0800

    Create generate_parentheses.go

---
+++ backtracking/generate_parentheses.go
@@ -0,0 +1,34 @@
+package main
+
+func generateParenthesis(n int) []string {
+	var result []string
+	var gen func(string, int)
+	gen = func(str string, maxLen int) {
+		if len(str) < maxLen {
+			gen(str+"(", maxLen)
+			gen(str+")", maxLen)
+		} else {
+			if validateParentheses(str) {
+				result = append(result, str)
+			}
+		}
+	}
+	gen("", n*2)
+	return result
+}
+
+func validateParentheses(str string) bool {
+	open := 0
+	for _, x := range str {
+		if x == '(' {
+			open++
+		} else if x == ')' {
+			if open > 0 {
+				open--
+			} else {
+				return false
+			}
+		}
+	}
+	return open == 0
+}
