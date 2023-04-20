3c39c0a1d3a55abf761425caacf4b75cbe21e3d2
stephenwong0707 <stephen91789443@gmail.com>
2023-04-20 22:41:14 +0800

    search range

---
+++ binarysearch/search_range.go
@@ -0,0 +1,41 @@
+func searchRange(nums []int, target int) []int {
+    first := searchFirst(nums, target)
+    last := searchLast(nums, target)
+    return []int{first, last}
+}
+
+func searchFirst(nums []int, target int) int {
+    idx := -1
+    low := 0
+    high := len(nums) - 1
+    for low <= high {
+        mid := low + (high - low) / 2
+        if nums[mid] == target {
+            idx = mid
+            high = mid - 1
+        } else if nums[mid] < target {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return idx
+}
+
+func searchLast(nums []int, target int) int {
+    idx := -1
+    low := 0
+    high := len(nums) - 1
+    for low <= high {
+        mid := low + (high - low) / 2
+        if nums[mid] == target {
+            idx = mid
+            low = mid + 1
+        } else if nums[mid] < target {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    return idx
+}
