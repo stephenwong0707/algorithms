eacfbb91bfff1293bd45a8ecfdbaffa7caa8b995
stephenwong0707 <stephen91789443@gmail.com>
2023-04-23 04:53:32 +0800

    binary search package

--- binarysearch/matrix.go
+++ binarysearch/matrix.go
@@ -1,31 +1,33 @@
+package binarysearch
+
 func searchMatrix(matrix [][]int, target int) bool {
-    row := 0
-    low := 0 
-    high := len(matrix) - 1
-    for (low <= high) {
-        mid := low + (high - low) / 2
-        if target < matrix[mid][0] {
-            high = mid - 1
-        } else if target > matrix[mid][len(matrix[0])-1] {
-            low = mid + 1
-        } else {
-            row = mid
-            break
-        }
-    }
+	row := 0
+	low := 0
+	high := len(matrix) - 1
+	for low <= high {
+		mid := low + (high-low)/2
+		if target < matrix[mid][0] {
+			high = mid - 1
+		} else if target > matrix[mid][len(matrix[0])-1] {
+			low = mid + 1
+		} else {
+			row = mid
+			break
+		}
+	}
 
-    low = 0 
-    high = len(matrix[0])-1
-    for (low <= high) {
-        mid := low + (high - low) / 2
-        if target < matrix[row][mid] {
-            high = mid - 1
-        } else if target > matrix[row][mid] {
-            low = mid + 1
-        } else {
-            return true
-        }
-    }
+	low = 0
+	high = len(matrix[0]) - 1
+	for low <= high {
+		mid := low + (high-low)/2
+		if target < matrix[row][mid] {
+			high = mid - 1
+		} else if target > matrix[row][mid] {
+			low = mid + 1
+		} else {
+			return true
+		}
+	}
 
-    return false
+	return false
 }
